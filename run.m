d9c88281d155810e80fd8f3083f55a5909de6822
Yojo41 <105660956+Yojo41@users.noreply.github.com>
2022-05-30 19:15:35 +0200

    Add files via upload
    run file.. still in progress

--- run.m
+++ run.m
@@ -1,27 +1,48 @@
 %% run.m
 
-function run(ourData)
-% rec = 1 for main vector  
-% rec = 2 for side vector 
-covmatrix = ourCov(ourData'); 
-test_cov = cov(ourData'); 
-
+function run(ourData,split)
+% Input: ourData 
+% split: 
+%calculation of the dimensions of the inputdata 
 dim = min(size(ourData)); 
-
-[dataMean, EigVec, EigVal] = our_pca(ourData,dim); 
-[coeff,score,latent] = pca(ourData'); 
-% Multiply the original data by the principal component vectors to get the 
-% projections of the original data on the principal component vector space.
-scores = (ourData - dataMean)' *  EigVec; 
-% scores will be orthogonal by construction, which you can check
-% corr(scores)
-
-recData = ((scores * EigVec')' + repmat(dataMean,1,50))';  
-%reconstructed = score * coeff' + repmat(dataMean, 1, 50)';
-
+len = length(ourData); 
+%% Exercise 1 
+covmatrix = ourCov(ourData'); 
+%test_cov = cov(ourData'); 
+%isalmost(covmatrix,test_cov,1e-10)
 if dim == 2
+    plot_ourCov(ourData'); 
+    title('covariance matrix')
+end 
+%% Exercise 2 (a)
+[dataMean, EigVec, EigVal] = our_pca(ourData,dim); 
+%[coeff,score,latent,tsquared,explained,mu] = pca(ourData','NumComponents',2); 
+%isalmost(abs(coeff),abs(EigVec),1e-10); 
+%% Exercise 2 (b) 
+%Multiply the original data by the principal component vectors to get the 
+%projections of the original data on the principal component vector space.
+%scores will be orthogonal by construction, which you can check corr(scores)
+scores = (ourData - dataMean)' *  EigVec; 
+%isalmost(abs(scores),abs(score),1e-10); 
+
+recData = ((scores * EigVec')' + repmat(dataMean,1,len))';  
+%reconstructed = score * coeff' + mu;
+%isalmost(abs(recData),abs(reconstructed),1e-10); 
+
 plot2DPCA(ourData',dataMean',recData,EigVec,EigVal',1,1)
-elseif dim == 3 
-plot3DPCA(origData',dataMean',recData,EigVec,EigVal',1,1)
+title('PCA 2D - original and reconstructed data')
+%% Exercise 3 
+if split == 1 
+    % going through the 1st,2nd,3rd.... pca component
+    for i = 1:dim
+scores_ms = (ourData - dataMean)' *  EigVec(:,i); 
+recData_ms = ((scores_ms * EigVec(:,i)')' + repmat(dataMean,1,len))'; 
+plot2DPCA(ourData',dataMean',recData_ms,EigVec,EigVal',1,1)
+title('main and side components of the PCA are used for reconstruction')
+    end 
+end 
+%% Exercise 4 (b) 
+if split == 2   
+plot3DPCA(ourData',dataMean',EigVec,EigVal',1,1)
 end 
 end 
